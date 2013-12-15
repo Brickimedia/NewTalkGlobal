241793b12c380d11e830e164839c9df248d4b9a1
Cody <codyn329@users.noreply.github.com>
2013-12-15 17:10:03 -0600

    Authoring Vietnamese translation

--- NewTalkGlobal.i18n.php
+++ NewTalkGlobal.i18n.php
@@ -4,10 +4,15 @@ $messages = array();
 
 $messages[ 'en' ] = array(
 		'newtalkglobal' => "newtalkglobal", 
-		'newtalkglobal-desc' => "Shows talk page messages recieved on any project of a wikifarm",
+		'newtalkglobal-desc' => "Shows talk page messages received on any project of a wikifarm",
 );
 
 $messages[ 'qqq' ] = array(
 		'newtalkglobal' => "The name of the extension",
 		'newtalkglobal-desc' => "{{desc}}",
+);
+
+$messages['vi'] = array(
+	        'newtalkglobal' => " nói chuyện mới toàn cầu",
+	        'newtalkglobal-desc' => "Hiển thị các thông trang thảo luận nhận được trên bất kỳ dự án của một trang trại wiki",
 );
