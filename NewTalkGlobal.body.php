9945405636603c0575652a18a042780a047b9dac
UltrasonicNXT <adamr_carter@btinternet.com>
2014-01-18 20:05:03 +0000

    Pretty names (+code style)

--- NewTalkGlobal.body.php
+++ NewTalkGlobal.body.php
@@ -1,45 +1,45 @@
 <?php
 
 class NewTalkGlobal {
-	
-	public static function onGetNewMessagesAlert( &$ntl, array $newtalks, User $user, OutputPage $out ){
-		
+
+	public static function onGetNewMessagesAlert( &$ntl, array $newtalks, User $user, OutputPage $out ) {
+
 		$ntl = '';
-		
+
 		global $newTalkGlobalDatabases;
-		
+
 		$msgs = array();
-		
+
 		foreach( $newTalkGlobalDatabases as $wiki => $data ){
 			$dbr = wfGetDB( DB_SLAVE, array(), $data['db'] );
-				
-			if( $user -> isAnon() ){
-				$res = $dbr -> select(
+
+			if( $user->isAnon() ){
+				$res = $dbr->select(
 						"user_newtalk",
 						"user_last_timestamp",
 						array( "user_ip" => $user -> getName() )
 				);
 			} else {
-				$res = $dbr -> select(
+				$res = $dbr->select(
 						"user_newtalk",
 						"user_last_timestamp",
 						array( "user_id" => $user -> getId() )
 				);
 			}
-				
-			if( $res -> numRows() != 0 ){
+
+			if( $res->numRows() != 0 ) {
 				$msgs[] = Xml::element(
 						'a',
 						array( 'href' => "{$data['url']}User_talk:$user" ),
-						$wiki
+						$data['name']
 				);
 			}
 		}
-		
+
 		$sep = str_replace( '_', ' ', wfMessage( 'newtalkseparator' )->escaped() );
 		$parts = implode( $sep, $msgs );
-		
-		if( count( $msgs )){
+
+		if( count( $msgs ) ) {
 			$ntl = wfMessage( 'youhavenewmessagesmulti' )->rawParams( $parts )->escaped();
 		}
 		return true;
