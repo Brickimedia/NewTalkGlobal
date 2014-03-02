43eb411f1886c9ae85c4ccd9bdc83da0d99779a5
George Barnick <george.barnick@brickcraft.me>
2014-03-01 22:31:06 -0500

    Adding Georgian (ქართული) internationalisation

--- NewTalkGlobal.i18n.php
+++ NewTalkGlobal.i18n.php
@@ -2,17 +2,34 @@
 
 $messages = array();
 
+/** English (English)
+ * @author UltrasonicNXT
+ */
 $messages[ 'en' ] = array(
-		'newtalkglobal' => "newtalkglobal", 
-		'newtalkglobal-desc' => "Shows talk page messages received on any project of a wikifarm",
+	'newtalkglobal' => 'newtalkglobal', 
+	'newtalkglobal-desc' => 'Shows talk page messages received on any project of a wikifarm',
 );
 
+/** Georgian (ქართული)
+ * @author ToaMeiko
+ */
+$messages['ka'] = array(
+	'newtalkglobal' => 'თოქ გლობალურ',
+	'newtalkglobal-desc' => 'მონიტორები გაიგო გვერდი შეტყობინებები მიღებული ნებისმიერი პროექტის ვიკი ფერმაში',
+);
+
+/** Message documentation (Message documentation)
+ * @author UltrasonicNXT
+ */
 $messages[ 'qqq' ] = array(
-		'newtalkglobal' => "The name of the extension",
-		'newtalkglobal-desc' => "{{desc}}",
+	'newtalkglobal' => 'The name of the extension',
+	'newtalkglobal-desc' => '{{desc}}',
 );
 
+/** Vietnamese (Việt)
+ * @author Codyn329
+ */
 $messages['vi'] = array(
-	        'newtalkglobal' => " nói chuyện mới toàn cầu",
-	        'newtalkglobal-desc' => "Hiển thị các thông trang thảo luận nhận được trên bất kỳ dự án của một trang trại wiki",
+	'newtalkglobal' => 'nói chuyện mới toàn cầu',
+	'newtalkglobal-desc' => 'Hiển thị các thông trang thảo luận nhận được trên bất kỳ dự án của một trang trại wiki',
 );
